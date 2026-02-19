6a1d5fa79a1c9e9aeae2292c1e8aa665fe05ce48
LianisDevs <lianisdevs@gmail.com>
2026-02-19 12:02:27 +1100

    update: user email_validator and requirements

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,5 @@
-from app.models.base_model import BaseModel
+from base_model import BaseModel
+from email_validator import validate_email, EmailNotValidError
 
 class User(BaseModel):
     """"User model"""
@@ -10,61 +11,64 @@ class User(BaseModel):
         self.email = email
         self.is_admin = is_admin
 
-        """Get Value for first name"""
-        @property
-        def first_name(self):
-            return self.__first_name
-        
-        """Set values for first name"""
-        @first_name.setter
-        def first_name(self, value):
-            if not value:
-                raise ValueError("First Name is required")
-            if len(value) > 50:
-                raise ValueError(
-                "First Name length must be "
-                "less than or equal to 50 characters")
-            if not isinstance(value, str):
-                raise TypeError("First Name must be a string")
-            self.__first_name = value
-        
-        """Get Value for last name"""
-        @property
-        def last_name(self):
-            return self._last_name
-                
-        """Set values for last name"""
-        @last_name.setter
-        def last_name(self, value):
-            if not value:
-                raise ValueError("Last Name is required")
-            if len(value) > 50:
-                raise ValueError(
-                "Last Name length must be "
-                "less than or equal to 50 characters")
-            if not isinstance(value, str):
-                raise TypeError("Last Name must be a string")
-            self.__last_name = value
+    """Get Value for first name"""
+    @property
+    def first_name(self):
+        return self.__first_name
 
-        """Get value for email"""
-        @property
-        def email(self):
-            return self.__email
-        
-        """Set value for email"""
-        @email.setter
-        def email(self, value):
-            """do more research on email validator"""
-            self.__email = value
-        
-        """Get value for admin"""
-        @property
-        def is_admin(self):
-            return self.__is_admin
-        
-        """Set value for admin"""
-        @is_admin.setter
-        def is_admin(self, value):
-            if not isinstance(value, bool) or isinstance(value, int):
-                raise TypeError("is_admin must be true or false")
-            self.__is_admin = value
+    """Set values for first name"""
+    @first_name.setter
+    def first_name(self, value):
+        if not value:
+            raise ValueError("First Name is required")
+        if len(value) > 50:
+            raise ValueError(
+            "First Name length must be "
+            "less than or equal to 50 characters")
+        if not isinstance(value, str):
+            raise TypeError("First Name must be a string")
+        self.__first_name = value
+ 
+    """Get Value for last name"""
+    @property
+    def last_name(self):
+        return self.__last_name
+
+    """Set values for last name"""
+    @last_name.setter
+    def last_name(self, value):
+        if not value:
+            raise ValueError("Last Name is required")
+        if len(value) > 50:
+            raise ValueError(
+            "Last Name length must be "
+            "less than or equal to 50 characters")
+        if not isinstance(value, str):
+            raise TypeError("Last Name must be a string")
+        self.__last_name = value
+
+    """Get value for email"""
+    @property
+    def email(self):
+        return self.__email
+ 
+    """Set value for email"""
+    @email.setter
+    def email(self, value):
+        try:
+            email_info = validate_email(value, check_deliverability=False)
+            self.__email = email_info
+        except EmailNotValidError:
+            raise EmailNotValidError("User email must be a valid email address")
+ 
+    """Get value for admin"""
+    @property
+    def is_admin(self):
+        return self.__is_admin
+ 
+    """Set value for admin"""
+    @is_admin.setter
+    def is_admin(self, value):
+        if not isinstance(value, bool) or isinstance(value, int):
+            raise TypeError("is_admin must be true or false")
+        self.__is_admin = value
