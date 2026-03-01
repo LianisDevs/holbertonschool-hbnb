305f91a6701902b303883e79b86cbdddab079a26
Lachie King <kingy00112233@gmail.com>
2026-03-01 23:10:44 +0000

    quick update of notes

--- app/api/v1/amenities.py
+++ app/api/v1/amenities.py
@@ -10,10 +10,6 @@ amenity_model = api.model('Amenity', {
 
 @api.route('/')
 class AmenityList(Resource):
-    @api.expect(amenity_model)
-    @api.response(201, 'Amenity successfully created')
-    @api.response(400, 'Amenity already exists')
-    @api.response(400, 'Invalid input data')
     def post(self):
         """Register a new amenity"""
         amenity_data = api.payload
@@ -30,7 +26,6 @@ class AmenityList(Resource):
         new_amenity = facade.create_amenity(amenity_data)
         return {'id': new_amenity.id, 'name': new_amenity.name}, 201
 
-    @api.response(200, 'List of amenities retrieved successfully')
     def get(self):
         """Retrieve a list of all amenities"""
         amenity_list = facade.get_all_amenities()
@@ -38,8 +33,6 @@ class AmenityList(Resource):
 
 @api.route('/<amenity_id>')
 class AmenityResource(Resource):
-    @api.response(200, 'Amenity details retrieved successfully')
-    @api.response(404, 'Amenity not found')
     def get(self, amenity_id):
         """Get amenity details by ID"""
         amenity = facade.get_amenity(amenity_id)
@@ -48,10 +41,6 @@ class AmenityResource(Resource):
 
         return {"id": amenity.id, "name": amenity.name}, 200
 
-    @api.expect(amenity_model)
-    @api.response(200, 'Amenity updated successfully')
-    @api.response(404, 'Amenity not found')
-    @api.response(400, 'Invalid input data')
     def put(self, amenity_id):
         """Update an amenity's information"""
         amenity_data = api.payload
