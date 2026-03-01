67519479bf69d141ade9709d290870a27ad4abb2
LianisDevs <lianisdevs@gmail.com>
2026-03-02 10:29:23 +1100

    Add: Review facade and tests

--- app/services/facade.py
+++ app/services/facade.py
@@ -42,27 +42,87 @@ class HBnBFacade:
     def get_place(self, place_id):
         pass
 
+
     def create_review(self, user_id, place_id, review_data):
+        """validates user and place and creates new review"""
         #check if user id is in database
         user = self.user_repo.get(user_id)
         if user is None:
             return None
+
         #check if place id is in database
         place = self.place_repo.get(place_id)
         if place is None:
             return None
+
         review = Review(review_data["text"], review_data["rating"], place.id, user.id)
+        self.review_repo.add(review)
         return review
 
 
+    def get_review(self, review_id):
+        """Fetches review, returns None if no matching review"""
+        review = self.review_repo.get(review_id)
+        if review is None:
+            return None
+        return review
+
+
+    def get_all_reviews(self):
+        """Fetches all reviews, returns None if no reviews or list of reviews"""
+
+        reviews = self.review_repo.get_all()
+        if reviews is None:
+            return []
+        return reviews
+
+
+    def get_reviews_by_place(self, place_id):
+        """
+        Fetches all reviews and filters by place_id
+        Returns empty list if no reviews or list of reviews
+        """
+        reviews = self.review_repo.get_all()
+        place_reviews = []
+        for val in reviews:
+            if val.place_id == place_id:
+                place_reviews.append(val)
+        return place_reviews
+
+
     def update_review(self, review_id, review_data):
-        pass
+        """
+        Fetches review- if no review returns None
+        Iterates through review_data, returns None if new keys are in review_data
+        Returns updated_review
+        """
+
+        review = self.get_review(review_id)
+        if review is None:
+            return None
+
+        for key in review_data.keys():
+            if key not in dir(review):
+                return None
+
+        self.review_repo.update(review_id, review_data)
+
+        updated_review = self.get_review(review_id)
+
+        return updated_review
+
 
     def delete_review(self, review_id):
-        pass
+        """
+        Fetches review- if no review returns None
+        Deletes review
+        """
+        review = self.get_review(review_id)
+        if review is None:
+            return None
 
-    def list_review(self, place_id):
-        pass
+        self.review_repo.delete(review_id)
+
 
     def create_amenity(self, amenity_data):
         amenity = Amenity(amenity_data["name"])
--- app/tests/services/facade_test.py
+++ app/tests/services/facade_test.py
@@ -2,6 +2,7 @@ import pytest
 
 from app.models.user import User
 from app.models.place import Place
+from app.services import facade
 from app.services.facade import HBnBFacade
 from app.models.review import Review
 from app.models.amenity import Amenity
@@ -10,8 +11,17 @@ class TestReviewClass():
     facade = HBnBFacade()
     user = User('john', 'smith', 'js@gmail.com', False)
     place = Place('House on a hill', 500, 37.7749, -122.4194, user)
+    place2 = Place('House on a haunted hill', 5003, 37.77349, -122.41194, user)
+    place3 = Place('Shack', 25, 27.77349, -42.41194, user)
+
 
     def test_create_review(self):
+        """
+        Test: create_review()
+        review is a Review class
+        Correct attributes
+        Values passed are on this instance
+        """
         review_data = {
             "text": "This was an amazing stay OMG",
             "rating": 5
@@ -24,22 +34,220 @@ class TestReviewClass():
         assert review.rating == 5
         assert review.user_id == self.user.id
         assert review.place_id == self.place.id
+        assert hasattr(review, "id")
 
-    def test_only_create_review_if_user_is_valid(self):
+
+    def test_create_review_if_user_is_not_valid(self):
+        """
+        Test: create_review()
+        If user is not valid- return None
+        """
         review_data = {
-            "text": "This was an amazing stay OMG",
-            "rating": 5
+            "text": "meh",
+            "rating": 3
         }
         review = self.facade.create_review(2, self.place.id, review_data)
         assert review == None
 
-    def test_only_create_review_if_place_is_valid(self):
+
+    def test_create_review_if_place_is_not_valid(self):
+        """
+        Test: create_review()
+        If place is not valid- return None
+        """
         review_data = {
-            "text": "This was an amazing stay OMG",
-            "rating": 5
+            "text": "meh",
+            "rating": 3
         }
         review = self.facade.create_review(self.user.id, 4, review_data)
         assert review == None
+
+
+    def test_get_review(self):
+        """
+        Test: get_review()
+        Creates new review then tests you can fetch this review by id
+        """
+        review_data = {
+            "text": "Horrible stay!",
+            "rating": 1
+        }
+        review = self.facade.create_review(self.user.id, self.place.id, review_data)
+        fetched_review = self.facade.get_review(review.id)
+        assert fetched_review.id == review.id
+
+
+    def test_get_review_if_review_is_not_valid(self):
+        """
+        Test: get_review()
+        If review is not valid- return None
+        """
+        review = self.facade.get_review(1)
+        assert review == None
+
+
+    def test_get_all_reviews(self):
+        """
+        Test: get_all_reviews()
+        Creates new empty facade, adds user, place and 3 reviews
+        Checks all 3 reviews just added are returned in list
+        """
+        empty_facade = HBnBFacade()
+        empty_facade.user_repo.add(self.user)
+        empty_facade.place_repo.add(self.place)
+
+        review_data_1 = {
+            "text": "This was an amazing stay OMG",
+            "rating": 5
+        }
+        review_data_2 = {
+            "text": "Horrible stay!",
+            "rating": 1
+        }
+        review_data_3 = {
+            "text": "Horrible bad bad stay!",
+            "rating": 1
+        }
+        empty_facade.place_repo.add(self.place2)
+
+        review_1 = empty_facade.create_review(self.user.id, self.place.id, review_data_1)
+        review_2 = empty_facade.create_review(self.user.id, self.place.id, review_data_2)
+        review_3 = empty_facade.create_review(self.user.id, self.place2.id, review_data_3)
+
+        reviews = empty_facade.get_all_reviews()
+        assert review_1 in reviews
+        assert review_2 in reviews
+        assert review_3 in reviews
+
+
+    def test_get_all_reviews_if_no_reviews(self):
+        """
+        Test: get_all_reviews()
+        Creates new empty facade, adds user and place
+        If no reviews in database- return empty list
+        """
+        empty_facade = HBnBFacade()
+        reviews = empty_facade.get_all_reviews()
+        assert reviews == []
+
+
+    def test_get_reviews_by_place(self):
+        """
+        Test: get_reviews_by_place()
+        Creates 3 reviews, 2 have place id 1 has place2 id
+        Checks that reviews returned for place is review_1 and review_2
+        """
+        review_data_1 = {
+            "text": "This was an amazing stay OMG",
+            "rating": 5
+        }
+        review_data_2 = {
+            "text": "Horrible stay!",
+            "rating": 1
+        }
+        review_data_3 = {
+            "text": "Horrible bad stay!",
+            "rating": 1
+        }
+
+        review_1 = self.facade.create_review(self.user.id, self.place.id, review_data_1)
+        review_2 = self.facade.create_review(self.user.id, self.place.id, review_data_2)
+        review_3 = self.facade.create_review(self.user.id, self.place2.id, review_data_3)
+
+        reviews = self.facade.get_reviews_by_place(self.place.id)
+        assert review_1 in reviews
+        assert review_2 in reviews
+        assert review_3 not in reviews
+
+
+    def test_get_reviews_by_place_if_place_has_no_reviews(self):
+        """
+        Test: get_reviews_by_place()
+        Creates new place, this place has no reviews
+        If no reviews with this place id- return empty list
+        """
+        # place with no reviews should retunr an empty array 
+        self.facade.place_repo.add(self.place3)
+        reviews = self.facade.get_reviews_by_place(self.place3.id)
+        assert reviews == []
+
+
+    def test_update_review(self):
+        """
+        Test: update_review()
+        Creates new review
+        Updates data on this review
+        Asserts that data is now updated values
+        """
+
+        review_data = {
+            "text": "What a view",
+            "rating": 5
+        }
+        review = self.facade.create_review(self.user.id, self.place.id, review_data)
+
+        new_review_data = {
+            "text": "Bad smell",
+            "rating": 4
+        }
+        updated_review = self.facade.update_review(review.id, new_review_data)
+
+        assert updated_review.text == "Bad smell"
+        assert updated_review.rating == 4
+
+
+    def test_update_review_if_review_is_not_valid(self):
+        """
+        Test: update_review()
+        If review id is not valid- returns None
+        """
+        review_data = {
+            "text": "Slept good",
+            "rating": 5
+        }
+        review = self.facade.update_review(2, review_data)
+        assert review == None
+
+
+    def test_update_review_if_review_data_is_not_valid(self):
+        """
+        Test: update_review()
+        If review id is not valid- returns None
+        """
+        review_data = {
+            "text": "What a view",
+            "rating": 5
+        }
+        review = self.facade.create_review(self.user.id, self.place.id, review_data)
+
+        updated_review_data = {
+            "abc": 5
+        }
+        review = self.facade.update_review(review.id, updated_review_data)
+        assert review == None
+
+
+    def test_delete_review(self):
+        """
+        Test: delete_review()
+        Creates new review
+        Deletes this review
+        Tries to get this review- return None
+        """
+        review_data = {
+            "text": "What a view",
+            "rating": 5
+        }
+        review = self.facade.create_review(self.user.id, self.place.id, review_data)
+        self.facade.delete_review(review.id)
+        deleted_review = self.facade.get_review(review.id)
+        assert deleted_review == None
+
+    def test_delete_review_if_review_is_not_valid(self):
+        self.facade.delete_review(1)
+        deleted_review = self.facade.get_review(1)
+        assert deleted_review == None
+
 
 class TestPlaceClass():
     facade = HBnBFacade()
