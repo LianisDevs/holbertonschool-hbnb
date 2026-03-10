185aa5b2c58fa048f891dc05e1969f01c1231fd7
LianisDevs <lianisdevs@gmail.com>
2026-03-10 12:37:47 +1100

    fix: handles errrors

--- app/utils/errors/review_errors.py
+++ app/utils/errors/review_errors.py
@@ -1,5 +1,3 @@
-
-
 class ReviewNotFoundError(Exception):
      def __init__(self, message="Review not found"):
         self.message = message
@@ -9,3 +7,9 @@ class ReviewInvalidDataError(Exception):
      def __init__(self, message="Review data invalid"):
         self.message = message
         super().__init__(self.message)
+
+class ReviewAlreadyExistsError(Exception):
+     def __init__(self, message="Review with this user_id and place_id already exists"):
+        self.message = message
+        super().__init__(self.message)
+
