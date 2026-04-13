afbe3af19eb09d6263ae95c6dae0189c3d58be0b
Lachie King <kingy00112233@gmail.com>
2026-04-13 11:03:10 +1000

    push so i can pull lol

--- part3/app/api/v1/reviews.py
+++ part3/app/api/v1/reviews.py
@@ -48,7 +48,7 @@ class Mock(Resource):
 class MockRestart(Resource):
     def delete(self):
         from run import get_app
-        database_path = '/home/liani/Code/holbertonschool-hbnb/instance/development.db'
+        database_path = 'Users/Kingy/HBnB/holbertonschool-hbnb/instance/development.db'
 
         if os.path.exists(database_path):
             os.remove(database_path)
